8228699668fed10a82915915d294a6c5a2124f2b
Divate <2085120@cognizant.com>
2022-03-10 13:38:41 +0530

    Docker Imagee

---
+++ WebApplication1Docker10/Controllers/ValuesController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1Docker10.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public string Get()
+        {
+            return ("Docker Image");
+        }
+    }
+}
